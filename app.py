95b7d632a83f784fde533c4bc6768469b23d14c7
sars06 <saradalaxmi1411@gmail.com>
2026-06-29 00:34:22 +0530

    Final Commit

--- app.py
+++ app.py
@@ -5,10 +5,21 @@ from sqlalchemy import or_
 
 app = Flask(__name__)
 import os
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+
+database_url = os.environ.get(
     "DATABASE_URL",
     "sqlite:///instance/placement.db"
 )
+
+if database_url.startswith("postgres://"):
+    database_url = database_url.replace(
+        "postgres://",
+        "postgresql://",
+        1
+    )
+
+app.config["SQLALCHEMY_DATABASE_URI"] = database_url
+
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "secret")
 
